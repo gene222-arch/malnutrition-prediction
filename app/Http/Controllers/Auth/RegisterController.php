09392e3bb9bb5f0e773e60d069a546e6655bab33
gene222-arch <genephillip222@gmail.com>
2021-12-20 16:37:51 +0800

    feat: update register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,9 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'available_person_on_absence' => ['required', 'string'],
+            'address' => ['required', 'string'],
+            'phone_number' => ['required', 'string']
         ]);
     }
 
@@ -69,6 +72,14 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $user
+            ->details()
+            ->create([
+                'available_person_on_absence' => $data['available_person_on_absence'],
+                'address' => $data['address'],
+                'phone_number' => $data['phone_number']
+            ]);
         
         $user->assignRole('Parent');
 
