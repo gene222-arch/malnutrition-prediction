19908e9e2d67b5f9937efaf7ef8b53eedde12714
gene222-arch <genephillip222@gmail.com>
2021-12-20 15:41:39 +0800

    feat: add malnutrition_symptoms table

---
+++ database/migrations/2021_12_20_072143_create_malnutrition_symptoms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMalnutritionSymptomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('malnutrition_symptoms', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('malnutrition_symptoms');
+    }
+}
