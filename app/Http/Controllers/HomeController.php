410a6df289acfd54faeb777d52390c6a73be9d4b
gene222-arch <genephillip222@gmail.com>
2021-12-22 06:13:42 +0800

    feat: implement dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -22,7 +23,13 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
-        return view('home');
+    {   
+        $user = Auth::user();
+
+        if ($user->hasRole('Administrator')) {
+            return view('pages.admin-dashboard');
+        }
+
+        return view('pages.dashboard');
     }
 }
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -7,14 +7,9 @@
   </div>
 	<div class="p-4">
 		<div class="row mb-2 justify-content-center align-items-center">
-			<img src="{{ asset('mulawin.png') }}" alt="" width="60" height="65">
 			<div class="col">
 				<h6><strong>{{ Auth::user()->name }}</strong></h6>
 				<small>{{ Auth::user()->email }}</small>
-			</div>
-			<div class="col mt-3">
-				<h3 class="text-warning">{{ Auth::user()->detail->loft_name }}</h3>
-				<span class=""><strong>{{ Auth::user()->detail->phone }}</strong></span>
 			</div>
 		</div>
       <ul class="list-unstyled components mb-5">
@@ -24,42 +19,6 @@
 			<li class="{{ request()->is('tournaments') ? 'active' : '' }}">
 				<a href="/tournaments"><i class="fas fa-poll-h mr-3"></i> Tournaments</a>
 			</li>
-			@hasrole('User')
-				<li class="{{ request()->is('my-pigeons') ? 'active' : '' }}">
-					<a href="/my-pigeons"><i class="fas fa-dove mr-3"></i> My Pigeons</a>
-				</li>
-			@endhasrole
-			<li class="{{ request()->is('coordinates') ? 'active' : '' }}">
-				<a href="/coordinates"><i class="fas fa-thumbtack mr-3"></i> View Coordinates</a>
-			</li>
-			@if (Auth::user()->hasRole('Admin'))
-				<li class="{{ request()->is('generate-qrcode') ? 'active' : '' }}">
-					<a href="/generate-qrcode"><i class="fas fa-qrcode mr-3"></i> Generate QR Code</a>
-				</li>
-			@endif
-			@hasrole('Admin')
-				<li class="{{ request()->is('clubs') ? 'active' : '' }}">
-					<a href="/clubs"><i class="fas fa-users mr-3"></i>Clubs</a>
-				</li>
-			@endhasrole
-			@hasrole('User')
-				<li class="{{ request()->is('clubs') ? 'active' : '' }}">
-					<a href="/clubs/{{ Auth::user()->club()->id }}">
-						<img 
-							class="img-responsive rounded mr-2" 
-							src="{{ Auth::user()->club()->logo_path }}" 
-							alt="Card image cap"
-							width="25"
-							height="25"
-						>
-						{{ Auth::user()->club()->name }}</a>
-				</li>
-			@endhasrole
-			@if (Auth::user()->hasRole('Admin'))
-				<li class="{{ request()->is('register') ? 'active' : '' }}">
-					<a href="/register"><i class="fas fa-user-plus mr-3"></i> Generate Player</a>
-				</li>
-			@endif
 			<li>
 				<a href="#" class="nav-link" onclick="document.getElementById('logout__form').submit()">
 					<p><i class="fas fa-sign-out-alt mr-3"></i>Logout</p>
